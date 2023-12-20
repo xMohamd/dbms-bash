c20bd5d02700bd32b361e71946888ec2152bde8c
Mohamed <10786768+xMohamd@users.noreply.github.com>
2023-12-20 21:55:37 +0200

    feat: add deleteFromTable to tbActions

---
+++ tbActions/deleteFromTable.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Path: tbActions/deleteFromTable.sh
+
+# Check if there are tables to insert into
+if [ ! "$(ls)" ] 
+then
+    echo "There are no tables to show."
+    exit
+fi
+
+# List the available tables using listTables.sh
+echo "Tables:"
+source ../../tbActions/listTables.sh
+
+# Ask the user to enter the table name then check if the table exists
+while true
+do
+    read -p "Enter the name of the table to delete from: " tbName
+    if [ ! -f "$tbName" ] || [ ! -f "$tbName.metadata" ]
+    then
+        echo "Table Doesn't Exist"
+        continue
+    fi
+    break
+done
+
+# determine the primary key
+pk=$(cat info.metadata | awk -F: '{print $1}' | tail -1)
+
+# detetmine the column number of the primary key
+pkNum=$(cat $tbName.metadata | awk -F: '{print $1}' | head -n -1 | grep -n "$pk" | cut -d: -f1)
+
+# Ask the user to enter the primary key value then check if the value exists
+while true
+do
+    read -p "Enter the value of the primary key ($pk) to delete: " pkValue
+    # search for the primary key value in the table if it's exist or not
+    if [ ! "$(awk -F: -v pkNum="$pkNum" -v pkValue="$pkValue" '$pkNum==pkValue' $tbName)" ]
+    then
+        echo "Value Doesn't Exist"
+        continue
+    fi
+
+
+    break
+done
+
+# delete the record with the primary key value that the user entered using sed
+sed -i "/^$pkValue/d" $tbName
