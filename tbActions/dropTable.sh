22dfa5baa6caf610303096b3a60dae08ccbbf846
Mohamed <10786768+xMohamd@users.noreply.github.com>
2023-12-20 17:09:42 +0200

    feat: add dropTable to tbActions

---
+++ tbActions/dropTable.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Path: tbActions/dropTable.sh
+
+# Check if there are tables to drop
+if [ ! "$(ls)" ] 
+then
+    echo "There are no tables to show."
+    exit
+fi
+
+# List the available tables using listTables.sh
+echo "Tables:"
+source ../../tbActions/listTables.sh
+
+
+# Ask the user to enter the table name then check if the table exists
+while true
+do
+    read -p "Enter the name of the table to drop: " tbName
+    if [ ! -f "$tbName" ] || [ ! -f "$tbName.metadata" ]
+    then
+        echo "Table Doesn't Exist"
+        continue
+    fi
+    break
+done
+
+# Delete the table file and the metadata file
+rm $tbName
+rm $tbName.metadata
